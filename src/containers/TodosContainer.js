421e28dd7850c311549580a560a578577cf3e21f
mochabbang <sungmin9036@gamil.com>
2022-09-14 08:46:35 +0900

    17장 리액트리덕스   - useSelector, useActions, useDispatch   - createAction, handleActions

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,71 @@
+import { useSelector } from 'react-redux';
+import Todos from '../components/Todos';
+import { changeInput, insert, toggle, remove } from '../modules/todos';
+import useActions from '../lib/useActions';
+
+const TodosContainer = () => {
+    const { input, todos } = useSelector(({ todos }) => ({
+        input: todos.input,
+        todos: todos.todos,
+    }));
+
+    const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+        [changeInput, insert, toggle, remove],
+        [],
+    );
+    // const dispatch = useDispatch();
+    // const onChangeInput = useCallback(
+    //     (input) => dispatch(changeInput(input)),
+    //     [dispatch],
+    // );
+    // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+    // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+    // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+    return (
+        <Todos
+            input={input}
+            todos={todos}
+            onChangeInput={onChangeInput}
+            onInsert={onInsert}
+            onToggle={onToggle}
+            onRemove={onRemove}
+        />
+    );
+};
+
+export default TodosContainer;
+
+/* As-Is */
+// const TodosContainer = ({
+//     input,
+//     todos,
+//     changeInput,
+//     insert,
+//     toggle,
+//     remove,
+// }) => {
+//     return (
+//         <Todos
+//             input={input}
+//             todos={todos}
+//             onChangeInput={changeInput}
+//             onInsert={insert}
+//             onToggle={toggle}
+//             onRemove={remove}
+//         />
+//     );
+// };
+
+// export default connect(
+//     ({ todos }) => ({
+//         input: todos.input,
+//         todos: todos.todos,
+//     }),
+//     {
+//         changeInput,
+//         insert,
+//         toggle,
+//         remove,
+//     },
+// )(TodosContainer);
